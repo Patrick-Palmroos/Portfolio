236d0708e6c83dca21e7ed1c5a8eb4b5e74a85dd
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-09-30 22:24:11 +0300

    Change: component hierarchy.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,14 @@
 import { test } from "./HeaderStyle";
+import { Container, Stack, Typography, Box } from "@mui/material";
 
 export default function Header() {
-  return <h1 style={test}>Hello!</h1>;
+  return (
+    <div style={{ margin: "0px" }}>
+      <Container component={"section"}>
+        <Box alignItems={"center"}>
+          <Typography sx={test}>Helloooo</Typography>
+        </Box>
+      </Container>
+    </div>
+  );
 }
