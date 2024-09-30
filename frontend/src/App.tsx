0638bdd5b9491df98651291a1ff53b2162823b1c
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-09-30 13:23:11 +0300

    Change: theme implementation.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 import { Button, Container, Stack, Typography, Box } from "@mui/material";
 import Header from "./components/Header.tsx";
+import { ThemeProvider } from "@emotion/react";
+import { createContext } from "react";
+import { darkTheme } from "./themes.ts";
+
+const ThemeContext = createContext("dark");
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Box component={"section"}>
-      <div>
-        <Header />
-      </div>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <ThemeContext.Provider value="dark">
+        <Box component={"section"} sx={{ backgroundColor: "common.black" }}>
+          <div>
+            <Header />
+          </div>
+        </Box>
+      </ThemeContext.Provider>
+    </ThemeProvider>
   );
 }
 
