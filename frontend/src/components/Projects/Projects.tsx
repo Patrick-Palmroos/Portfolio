7d54487f3c49caec72c6cb7b7b61838ad06e3fe1
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-03 17:13:25 +0300

    Add: interface for project.

--- frontend/src/components/Projects/Projects.tsx
+++ frontend/src/components/Projects/Projects.tsx
@@ -1,12 +1,13 @@
 import { Stack, Typography, Box } from "@mui/material";
 import ProjectJson from "../util/projects.json";
+import Project from "../util/ProjectInterface";
 
 export default function Projects() {
   return (
     <div>
       <Stack direction={"column"}>
         <Stack direction={"row"}>
-          {ProjectJson.projects.map((project) => (
+          {ProjectJson.projects.map((project: Project) => (
             <Box key={project.id}>
               <Typography>{project.name}</Typography>
             </Box>
