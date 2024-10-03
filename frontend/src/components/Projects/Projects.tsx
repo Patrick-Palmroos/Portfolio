7328444a8ea8e3b4b3fa7904b7f275be905e1b75
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-04 00:21:21 +0300

    Change: page system added.

--- frontend/src/components/Projects/Projects.tsx
+++ frontend/src/components/Projects/Projects.tsx
@@ -2,13 +2,28 @@ import { Stack, Typography, Box } from "@mui/material";
 import ProjectJson from "../util/projects.json";
 import Project from "../util/ProjectInterface";
 import ProjectBox from "../util/ProjectBox/ProjectBox";
+import { useState } from "react";
 
 export default function Projects() {
+  const [page, setPage] = useState(1);
+
+  const projectCount: number = ProjectJson.projects.length;
+
+  //start index counted as in page 2 would be (2-1)*4=4
+  const startIndex: number = (page - 1) * 4;
+  //endIndex counted as in (4+4(=8), 6) = 6
+  const endIndex: number = Math.min(startIndex + 4, projectCount);
+  //slicing list into a new list.
+  const visibleList: Project[] = ProjectJson.projects.slice(
+    startIndex,
+    endIndex
+  );
+
   return (
     <div>
       <Stack direction={"column"}>
         <Stack direction={"row"}>
-          {ProjectJson.projects.map((project: Project) => (
+          {visibleList.map((project: Project) => (
             <Box key={project.id}>
               <ProjectBox project={project} />
             </Box>
