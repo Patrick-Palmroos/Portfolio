1b0a15f07d944f887f5175b8236c879b3db6ebfe
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-03 15:06:23 +0300

    Change: Title added to App.tsx.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button, Container, Stack, Typography, Box } from "@mui/material";
 import Header from "./components/Header/Header.tsx";
 import TopBar from "./components/TopBar/TopBar.tsx";
+import { Title } from "./components/util/Title/Title.tsx";
 import { ThemeProvider } from "@emotion/react";
 import { createContext } from "react";
 import { darkTheme } from "./themes.ts";
@@ -23,6 +24,7 @@ function App() {
           >
             <TopBar />
             <Header />
+            <Title />
           </Box>
         </div>
       </ThemeContext.Provider>
