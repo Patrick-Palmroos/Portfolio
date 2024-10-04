a37dc21882e64f373355dcec01c84d8d460f16f9
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-04 10:32:07 +0300

    Change: Image styling.

--- frontend/src/components/util/ProjectBox/ProjectBox.tsx
+++ frontend/src/components/util/ProjectBox/ProjectBox.tsx
@@ -1,6 +1,6 @@
 import Project from "../ProjectInterface";
 import { Stack, Typography, Box } from "@mui/material";
-import { projectBoxStyle } from "./ProjectBoxStyle";
+import { projectBoxStyle, imageStyle } from "./ProjectBoxStyle";
 
 export default function ProjectBox({ project }: { project: Project }) {
   return (
@@ -8,6 +8,7 @@ export default function ProjectBox({ project }: { project: Project }) {
       <Stack direction={"column"} sx={projectBoxStyle}>
         <Typography>{project.name}</Typography>
         <Typography>{project.subtitle}</Typography>
+        <img src={project.image} alt="picture of software" style={imageStyle} />
       </Stack>
     </div>
   );
--- frontend/src/components/util/ProjectBox/ProjectBoxStyle.ts
+++ frontend/src/components/util/ProjectBox/ProjectBoxStyle.ts
@@ -1,7 +1,15 @@
 import CSS from "csstype";
 
 export const projectBoxStyle: CSS.Properties = {
+  position: "relative",
   backgroundColor: "red",
   width: "30.25rem",
   height: "14.4rem",
+};
+
+export const imageStyle: CSS.Properties = {
+  position: "absolute",
+  right: "0rem",
+  top: "1rem",
+  height: "13rem",
 };
