2709f53f5a65d1a5b6f90323ea5213b2aa1bde12
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-07 07:35:40 +0300

    Remove: old imports.

--- frontend/src/components/util/ArrowButton/ArrowButton.tsx
+++ frontend/src/components/util/ArrowButton/ArrowButton.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography, Box } from "@mui/material";
+import { Box } from "@mui/material";
 
 export default function ArrowButton({ callback }: { callback: () => void }) {
   return (
