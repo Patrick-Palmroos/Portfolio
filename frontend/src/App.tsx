12a73457040313cba9663d5f7537e95e12bf881e
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-08 22:37:02 +0300

    Change: added about me to App.tsx.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header.tsx";
 import TopBar from "./components/TopBar/TopBar.tsx";
 import Projects from "./components/Projects/Projects.tsx";
 import Title from "./components/util/Title/Title.tsx";
+import AboutMe from "./components/AboutMe/AboutMe.tsx";
 import { ThemeProvider } from "@emotion/react";
 import { createContext } from "react";
 import { darkTheme } from "./themes.ts";
@@ -27,6 +28,7 @@ function App() {
             <Stack direction="row">
               <Stack>
                 <Title title="About me" line={false} />
+                <AboutMe />
               </Stack>
             </Stack>
           </Box>
