ccf8027ba3fedd6673747dcfb377635d981c4c9f
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-08 22:58:49 +0300

    Change: about me styling.

--- frontend/src/components/AboutMe/AboutMe.tsx
+++ frontend/src/components/AboutMe/AboutMe.tsx
@@ -1,21 +1,24 @@
 import { Typography, Box } from "@mui/material";
+import { boxStyle } from "./AboutMeStyle";
 
 export default function AboutMe() {
   return (
     <div>
-      <Box>
-        <Typography>
+      <Box sx={boxStyle}>
+        <Typography variant="h2" sx={{ fontSize: "25px" }}>
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
           accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
           ab illo inventore veritatis et quasi architecto beatae vitae dicta
           sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
           aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-          ea commodi consequatur? Quis autem vel eum
+          qui ratione voluptatem sequi nesciunt.
+          <br />
+          <br /> Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+          amet, consectetur, adipisci velit, sed quia non numquam eius modi
+          tempora incidunt ut labore et dolore magnam aliquam quaerat
+          voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem
+          ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+          consequatur? Quis autem vel eum
         </Typography>
       </Box>
     </div>
--- frontend/src/components/AboutMe/AboutMeStyle.ts
+++ frontend/src/components/AboutMe/AboutMeStyle.ts
@@ -1,3 +1,10 @@
 import CSS from "csstype";
 
-export const boxStyle: CSS.Properties = {};
+export const boxStyle: CSS.Properties = {
+  maxWidth: "31rem",
+  marginLeft: "9rem",
+  marginTop: "2rem",
+  borderRadius: "20px",
+  boxShadow: "inset 0 0 10px 0px rgba(0, 0, 0, 0.75)",
+  padding: "2rem",
+};
