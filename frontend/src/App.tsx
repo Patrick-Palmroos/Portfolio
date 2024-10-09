2b97d94ba143b6b0e64bb7ad74ff2eae6bdbb206
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-09 16:10:15 +0300

    Change: added Skills to App.tsx.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Projects from "./components/Projects/Projects.tsx";
 import Title from "./components/util/Title/Title.tsx";
 import AboutMe from "./components/AboutMe/AboutMe.tsx";
 import Line from "./components/util/Line/Line.tsx";
+import Skills from "./components/Skills/Skills.tsx";
 import { ThemeProvider } from "@emotion/react";
 import { createContext } from "react";
 import { darkTheme } from "./themes.ts";
@@ -43,6 +44,7 @@ function App() {
               <Line maxHeight={"1rem"} />
               <Stack>
                 <Title title="Skills" line={false} />
+                <Skills />
               </Stack>
             </Stack>
           </Box>
