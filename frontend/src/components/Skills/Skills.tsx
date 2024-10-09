b58ed426ef8c1aefe6363f720c8bcb36a841353c
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-09 16:09:22 +0300

    Change: added Skills function.

---
+++ frontend/src/components/Skills/Skills.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from "@mui/material";
+
+export default function Skills() {
+  return (
+    <div>
+      <Stack>
+        <Typography>Hellloooooo</Typography>
+      </Stack>
+    </div>
+  );
+}
