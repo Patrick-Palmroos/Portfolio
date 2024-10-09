4f3bf56deabb791153ad4c1578e156327aa75c53
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-09 18:20:56 +0300

    Change: Seperated skills into top, middle and bottom parts.

--- frontend/src/components/Skills/Skills.tsx
+++ frontend/src/components/Skills/Skills.tsx
@@ -5,10 +5,28 @@ import { Skill } from "../util/Interfaces";
 import SkillBox from "../util/SkillBox/SkillBox";
 
 export default function Skills() {
+  const skillsList = SkillsJson.skills;
+  const topRow = skillsList.slice(0, 3);
+  const theMiddle = skillsList.slice(3, skillsList.length - 3);
+  const bottomRow = skillsList.slice(skillsList.length - 3, skillsList.length);
+  console.log(theMiddle);
   return (
     <div>
-      <Box sx={gridStyle}>
-        {SkillsJson.skills.map((skill: Skill) => (
+      {/*TopRow*/}
+      <Box sx={{ ...gridStyle, gridTemplateColumns: "repeat(3, 4rem)" }}>
+        {topRow.map((skill: Skill) => (
+          <SkillBox skill={skill} />
+        ))}
+      </Box>
+      {/*The middle parts*/}
+      <Box sx={{ ...gridStyle, gridTemplateColumns: "repeat(5, 4rem)" }}>
+        {theMiddle.map((skill: Skill) => (
+          <SkillBox skill={skill} />
+        ))}
+      </Box>
+      {/*Bottom Row*/}
+      <Box sx={{ ...gridStyle, gridTemplateColumns: "repeat(3, 4rem)" }}>
+        {bottomRow.map((skill: Skill) => (
           <SkillBox skill={skill} />
         ))}
       </Box>
--- frontend/src/components/Skills/SkillsStyle.ts
+++ frontend/src/components/Skills/SkillsStyle.ts
@@ -2,6 +2,5 @@ import CSS from "csstype";
 
 export const gridStyle: CSS.Properties = {
   display: "grid",
-  gridTemplateColumns: "repeat(4, 4rem)",
   gridAutoRows: "4rem",
 };
