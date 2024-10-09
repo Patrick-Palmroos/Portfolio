2821449474a797e106f4f562950edec81c46e1e1
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-09 17:09:32 +0300

    Add: SkillsStyle.ts and demo styling for the grid.

--- frontend/src/components/Skills/Skills.tsx
+++ frontend/src/components/Skills/Skills.tsx
@@ -1,11 +1,23 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack, Typography, Box } from "@mui/material";
+import SkillsJson from "../util/skills.json";
+import { gridStyle } from "./SkillsStyle";
 
 export default function Skills() {
   return (
     <div>
-      <Stack>
-        <Typography>Hellloooooo</Typography>
-      </Stack>
+      <Box sx={gridStyle}>
+        {SkillsJson.skills.map((skill) => (
+          <Box
+            key={skill.id}
+            sx={{
+              gridColumn: `span ${skill.width}`,
+              gridRow: `span ${skill.height}`,
+              backgroundColor: "white",
+              boxShadow: "inset 0 0 10px 2px rgba(0, 0, 0, 0.75)",
+            }}
+          />
+        ))}
+      </Box>
     </div>
   );
 }
---
+++ frontend/src/components/Skills/SkillsStyle.ts
@@ -0,0 +1,7 @@
+import CSS from "csstype";
+
+export const gridStyle: CSS.Properties = {
+  display: "grid",
+  gridTemplateColumns: "repeat(4, 4rem)",
+  gridAutoRows: "4rem",
+};
