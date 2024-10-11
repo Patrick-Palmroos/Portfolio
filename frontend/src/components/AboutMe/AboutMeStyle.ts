0233eda22f4db8934b451966dc3facff9893b891
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-11 15:45:42 +0300

    Change: about me box now has border

--- frontend/src/components/AboutMe/AboutMeStyle.ts
+++ frontend/src/components/AboutMe/AboutMeStyle.ts
@@ -7,6 +7,9 @@ export const boxStyle: CSS.Properties = {
   marginTop: "2rem",
   marginBottom: "10rem",
   borderRadius: "20px",
-  boxShadow: "inset 0 0 10px 0px rgba(0, 0, 0, 0.75)",
+  boxShadow: "inset 0 0 10px 3px rgba(0, 0, 0, 0.75)",
+  borderStyle: "solid",
+  borderWidth: "2px 4px",
+  borderColor: "primary.light",
   padding: "2rem",
 };
