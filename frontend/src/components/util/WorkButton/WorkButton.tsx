d24ffa4fd2394be2c7268e50336d05f7d1f467f0
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-13 00:29:35 +0300

    Change: default implementation for WorkButton.tsx.

---
+++ frontend/src/components/util/WorkButton/WorkButton.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+
+export default function WorkButton({
+  width,
+  height,
+}: {
+  width: number;
+  height: number;
+}) {
+  return (
+    <div>
+      <Box>
+        <h2>HALUUUU</h2>
+      </Box>
+    </div>
+  );
+}
