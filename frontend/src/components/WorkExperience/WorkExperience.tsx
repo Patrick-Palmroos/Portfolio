991fa4704abde89956488c5f9de622c10bccf9d1
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-13 00:57:53 +0300

    Change: callback function created.

--- frontend/src/components/WorkExperience/WorkExperience.tsx
+++ frontend/src/components/WorkExperience/WorkExperience.tsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import { Box, Stack } from "@mui/material";
 import workJson from "../util/work.json";
 import WorkButton from "../util/WorkButton/WorkButton";
 
 export default function WorkExperience() {
-  const work = workJson.work;
+  const [work, setWork] = useState<number>(0);
+  const workList = workJson.work;
+
+  const activeWork = () => {};
 
   return (
     <div>
       <Box>
         <Stack direction="row">
           <Stack>
-            {work.map((work) => (
-              <WorkButton width={5} height={5} work={work} key={work.id} />
+            {workList.map((work) => (
+              <WorkButton
+                width={5}
+                height={5}
+                work={work}
+                key={work.id}
+                callback={activeWork}
+              />
             ))}
           </Stack>
         </Stack>
