516830261a041b732c948f0164abdb2f64bcee9a
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-13 00:34:25 +0300

    Change: display workJson file data for testing.

--- frontend/src/components/WorkExperience/WorkExperience.tsx
+++ frontend/src/components/WorkExperience/WorkExperience.tsx
@@ -1,11 +1,19 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import workJson from "../util/work.json";
 
 export default function WorkExperience() {
+  const work = workJson.work;
+
   return (
     <div>
       <Box>
-        <h1>HELLO</h1>
+        <Stack direction="row">
+          <Stack>
+            {work.map((work) => (
+              <h1>{work.name}</h1>
+            ))}
+          </Stack>
+        </Stack>
       </Box>
     </div>
   );
