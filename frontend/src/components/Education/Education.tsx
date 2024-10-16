2d166e6912bd4bf1a344d844b77ccd2fbd66d79f
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-16 20:41:46 +0300

    Change: gave Education.tsx default implementation.

---
+++ frontend/src/components/Education/Education.tsx
@@ -0,0 +1,11 @@
+import { Box, Stack } from "@mui/material";
+
+export default function Education() {
+  return (
+    <div>
+      <Stack>
+        <Box>HELLOOOOOOOOO</Box>
+      </Stack>
+    </div>
+  );
+}
