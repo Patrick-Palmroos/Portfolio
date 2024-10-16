d50be3fafc9968d49737a0e1b5752a5f0249424a
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-16 12:21:42 +0300

    Change: border to have gradient.

--- frontend/src/components/WorkExperience/WorkExperienceStyle.ts
+++ frontend/src/components/WorkExperience/WorkExperienceStyle.ts
@@ -14,9 +14,13 @@ export const detailBox: CSS.Properties = {
   right: "12rem",
   borderRadius: "20px",
   boxShadow: "inset 0 0 10px 3px rgba(0, 0, 0, 0.75)",
-  borderStyle: "solid",
+  border: "solid transparent",
   borderWidth: "3px 4px",
-  borderColor: "primary.light",
+  background:
+    "linear-gradient(#27242E, #27242E) padding-box, linear-gradient(to right, #FF74D0, #B985FC) border-box",
+  //borderImageSlice: 1,
+  //borderColor: "primary.light",
+  //borderImageSource: "linear-gradient(to right, white, black) 1",
   marginTop: "-5.5rem",
   padding: "1.5rem",
 };
