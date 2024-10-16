dc7e7eee1c0e94ed424e9d9e9a58fbfd2e2e1fea
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-16 15:36:08 +0300

    Change: AboutMe box has gradient now.

--- frontend/src/components/AboutMe/AboutMeStyle.ts
+++ frontend/src/components/AboutMe/AboutMeStyle.ts
@@ -8,8 +8,9 @@ export const boxStyle: CSS.Properties = {
   marginBottom: "10rem",
   borderRadius: "20px",
   boxShadow: "inset 0 0 10px 3px rgba(0, 0, 0, 0.75)",
-  borderStyle: "solid",
-  borderWidth: "2px 4px",
-  borderColor: "primary.light",
+  border: "solid transparent",
+  borderWidth: "6px",
+  background:
+    "linear-gradient(#27242E, #27242E) padding-box, linear-gradient(to right, #FF74D0, #B985FC) border-box",
   padding: "2rem",
 };
