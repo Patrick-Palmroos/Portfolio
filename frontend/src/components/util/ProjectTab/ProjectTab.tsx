4bc18b58351759c20ebf8ac7b32d95f65e3f3386
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-17 23:59:37 +0300

    Change: stylings to ProjectTabStyle.ts.

--- frontend/src/components/util/ProjectTab/ProjectTab.tsx
+++ frontend/src/components/util/ProjectTab/ProjectTab.tsx
@@ -1,17 +1,11 @@
 import { Box } from "@mui/material";
+import { projectTabStyle } from "./ProjectTabStyle";
 
 export default function ProjectTab() {
   return (
     <Box
       sx={{
-        content: '""',
-        width: "40%",
-        height: "80%",
-        background: "red",
-        position: "fixed",
-        left: "30%",
-        top: "15%",
-        zIndex: 9999,
+        projectTabStyle,
       }}
     />
   );
---
+++ frontend/src/components/util/ProjectTab/ProjectTabStyle.ts
@@ -0,0 +1,13 @@
+import CSS from "csstype";
+
+export const projectTabStyle: CSS.Properties = {
+  content: '""',
+  borderRadius: "20px",
+  width: "60%",
+  height: "70%",
+  backgroundColor: "primary.light",
+  position: "fixed",
+  left: "20%",
+  top: "20%",
+  zIndex: 9999,
+};
