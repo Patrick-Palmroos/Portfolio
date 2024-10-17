8d4defc6d68a8815ff8f10d5eb4f64b571748ccb
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-17 12:28:48 +0300

    Change: added pp.png to topbar and made topbars position fixed.

--- frontend/src/components/TopBar/TopBar.tsx
+++ frontend/src/components/TopBar/TopBar.tsx
@@ -1,15 +1,43 @@
-import { Stack } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import LayerButton from "../util/LayerButton/LayerButton";
 
 export default function TopBar() {
   return (
     <div>
       <Stack
+        position={"relative"}
         component={"section"}
         paddingTop={"1.6rem"}
         justifyContent={"center"}
         direction={"row"}
+        sx={{
+          background: "#1F1E2C",
+          position: "fixed",
+          width: "100vw",
+          paddingBottom: "0.5rem",
+          paddingTop: "1rem",
+          zIndex: 10,
+        }}
       >
+        <Box
+          sx={{
+            //background: "black",
+            width: "5rem",
+            height: "5rem",
+            position: "absolute",
+            left: "2rem",
+            top: "1rem",
+          }}
+        >
+          <img
+            src="/pp.png"
+            alt="logo"
+            style={{
+              width: "4rem",
+              height: "4rem",
+            }}
+          ></img>
+        </Box>
         <LayerButton title="Projects" link="#projects" />
         <LayerButton title="About me" link="#aboutMe" />
         <LayerButton title="Skills" link="#skills" />
