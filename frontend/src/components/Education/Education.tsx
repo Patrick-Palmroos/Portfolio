b550eb9e65837881411e6efa6e23c9b0aa069bd3
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-17 11:06:14 +0300

    Change: added styling for two boxes.

--- frontend/src/components/Education/Education.tsx
+++ frontend/src/components/Education/Education.tsx
@@ -1,12 +1,36 @@
 import { Box, Stack, Typography } from "@mui/material";
-import { container } from "./EducationStyle";
+import { container, educationBox, educationBackdrop } from "./EducationStyle";
 
 export default function Education() {
   return (
     <div>
-      <Stack direction={"row"} sx={container} spacing={4}>
-        <Box>HELLLOOOOOO</Box>
-        <Box>POOOKIIIIIEE</Box>
+      <Stack direction={"row"} sx={container} spacing={6}>
+        <Box position={"relative"}>
+          <Box
+            sx={{
+              ...educationBackdrop,
+              "&::before": {
+                ...educationBox,
+              },
+            }}
+          >
+            <Typography zIndex={3} position={"relative"}>
+              HELLO
+            </Typography>
+          </Box>
+        </Box>
+        <Box position={"relative"}>
+          <Box
+            sx={{
+              ...educationBackdrop,
+              "&::before": {
+                ...educationBox,
+              },
+            }}
+          >
+            HELLLOOOOOO
+          </Box>
+        </Box>
       </Stack>
     </div>
   );
--- frontend/src/components/Education/EducationStyle.ts
+++ frontend/src/components/Education/EducationStyle.ts
@@ -3,5 +3,28 @@ import CSS from "csstype";
 export const container: CSS.Properties = {
   display: "flex",
   flexDirection: "row",
-  justifyContent: "center",
+  width: "100vw",
+  justifyContent: "space-evenly",
+};
+
+export const educationBox: CSS.Properties = {
+  content: '""',
+  position: "absolute",
+  left: "1rem",
+  top: "1rem",
+  width: "25rem",
+  height: "9rem",
+  borderRadius: "20px",
+  padding: "1rem",
+  background: "blue",
+  zIndex: 2,
+};
+
+export const educationBackdrop: CSS.Properties = {
+  width: "25rem",
+  height: "9rem",
+  borderRadius: "20px",
+  padding: "1rem",
+  background: "red",
+  zIndex: 1,
 };
