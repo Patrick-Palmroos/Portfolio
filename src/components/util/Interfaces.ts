85b41cbbcaf6fbd7326976f6d11fcbb472d30c30
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-26 18:18:50 +0300

    Change: Project interface.

--- src/components/util/Interfaces.ts
+++ src/components/util/Interfaces.ts
@@ -2,11 +2,14 @@ export interface Project {
   id: number;
   name: string;
   logos: string[];
-  subtitle: string;
+  subtitleEn: string;
+  subtitleFi: string;
   mobileImages: boolean;
   images: string[];
-  duration: string;
-  details: string;
+  durationEn: string;
+  detailsEn: string;
+  durationFi: string;
+  detailsFi: string;
   employer: string;
   backDrop: string;
 }
