a1872d53397155818f5331701bc7d76c1532f658
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-10-28 19:52:47 +0200

    Change: ContactButton basic implementation.

--- src/components/util/ContactButton/ContactButton.tsx
+++ src/components/util/ContactButton/ContactButton.tsx
@@ -1,1 +1,19 @@
-export default function ContactButton() {}
+import { Box, Typography } from "@mui/material";
+
+export default function ContactButton({
+  title,
+  link,
+}: {
+  title: string;
+  link: string;
+}) {
+  return (
+    <div>
+      <Box
+        component={"a"}
+        href={link}
+        sx={{ width: "2rem", height: "2rem", background: "red" }}
+      ></Box>
+    </div>
+  );
+}
