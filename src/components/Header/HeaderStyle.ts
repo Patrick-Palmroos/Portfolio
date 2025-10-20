59d34427fe3dfd944d7b85b35e1c7630d0c01d76
Patrick-Palmroos <palmroos.patrick@gmail.com>
2025-10-20 12:31:46 +0300

    Update: site.

--- src/components/Header/HeaderStyle.ts
+++ src/components/Header/HeaderStyle.ts
@@ -1,56 +1,56 @@
-import CSS from "csstype";
+import CSS from 'csstype';
 
 export const backgroundBox: CSS.Properties = {
-  height: "33rem",
-  maxWidth: "90rem",
-  width: "95%",
-  margin: "0px 3rem 0px 2rem",
-  position: "relative",
-  boxShadow: "0rem 0.6rem 0px 0px rgb(40, 26, 40)",
-  overflow: "hidden",
-  borderRadius: "10px",
-  backgroundColor: "#27242E",
-  zIndex: 0,
+  height: '33rem',
+  maxWidth: '90rem',
+  width: '95%',
+  margin: '0px 3rem 0px 2rem',
+  position: 'relative',
+  boxShadow: '0rem 0.6rem 0px 0px rgb(40, 26, 40)',
+  overflow: 'hidden',
+  borderRadius: '10px',
+  backgroundColor: '#27242E',
+  zIndex: 0
 };
 
 export const circle: CSS.Properties = {
-  position: "absolute",
-  width: "35.5rem",
-  height: "35.5rem",
-  backgroundColor: "transparent",
-  borderRadius: "50%",
-  right: "-18rem",
-  bottom: "-22rem",
-  boxShadow: "-0.45rem 0.5rem 0.9rem rgb(40, 26, 40)",
-  overflow: "hidden",
-  zIndex: 1,
+  position: 'absolute',
+  width: '35.5rem',
+  height: '35.5rem',
+  backgroundColor: 'transparent',
+  borderRadius: '50%',
+  right: '-18rem',
+  bottom: '-22rem',
+  boxShadow: '-0.45rem 0.5rem 0.9rem rgb(40, 26, 40)',
+  overflow: 'hidden',
+  zIndex: 1
 };
 
 export const pfpStyle: CSS.Properties = {
-  position: "absolute",
-  right: "4rem",
-  bottom: "1rem",
-  width: "32rem",
-  height: "auto",
-  zIndex: 2,
+  position: 'absolute',
+  right: '0rem',
+  bottom: '1rem',
+  width: '32rem',
+  height: 'auto',
+  zIndex: 2
 };
 
 export const logoStyle: CSS.Properties = {
-  position: "absolute",
-  width: "39.8rem",
-  left: "0.5rem",
-  bottom: "0rem",
-  zIndex: -3,
+  position: 'absolute',
+  width: '39.8rem',
+  left: '0.5rem',
+  bottom: '0rem',
+  zIndex: -3
 };
 
 export const titleBox: CSS.Properties = {
-  position: "relative",
-  backgroundColor: "color(srgb 0 0 0 / 0.7)",
-  padding: "1rem",
-  paddingRight: "2rem",
-  width: "40vw",
-  left: "-3rem",
-  borderRadius: "1rem",
-  top: "0",
-  zIndex: 4,
+  position: 'relative',
+  backgroundColor: 'color(srgb 0 0 0 / 0.7)',
+  padding: '1rem',
+  paddingRight: '2rem',
+  width: '40vw',
+  left: '-3rem',
+  borderRadius: '1rem',
+  top: '0',
+  zIndex: 4
 };
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,169 +1,145 @@
-import { Box } from "@mui/material";
-import SkillsJson from "../util/skills.json";
-import { gridStyle } from "./SkillsStyle";
-import { Skill } from "../util/Interfaces";
-import SkillBox from "../util/SkillBox/SkillBox";
-import ScrollAnimation from "react-animate-on-scroll";
-import { useMediaQuery } from "react-responsive";
-import FilterButton from "../util/FilterButton/FilterButton";
-import { useState, useEffect } from "react";
+import { Box } from '@mui/material';
+import SkillsJson from '../util/skills.json';
+import { gridStyle } from './SkillsStyle';
+import { Skill } from '../util/Interfaces';
+import SkillBox from '../util/SkillBox/SkillBox';
+import ScrollAnimation from 'react-animate-on-scroll';
+import { useMediaQuery } from 'react-responsive';
+import FilterButton from '../util/FilterButton/FilterButton';
+import { useState, useEffect } from 'react';
 
 export default function Skills() {
-    const isDesktop = useMediaQuery({ query: "(min-width: 1224px)" });
-    const isMobile = useMediaQuery({ query: "(min-width: 730px)" });
-    const isVeryTiny = useMediaQuery({ query: "(min-width: 550px)" });
-    const [activeTag, setActiveTag] = useState<string>("");
-    const skillsList = SkillsJson.skills;
-    const topRow = skillsList.slice(0, 2);
-    const theMiddle = skillsList.slice(2, skillsList.length - 3);
-    const bottomRow = skillsList.slice(
-        skillsList.length - 3,
-        skillsList.length
-    );
+  const isDesktop = useMediaQuery({ query: '(min-width: 1224px)' });
+  const isMobile = useMediaQuery({ query: '(min-width: 730px)' });
+  const isVeryTiny = useMediaQuery({ query: '(min-width: 550px)' });
+  const [activeTag, setActiveTag] = useState<string>('');
+  const skillsList = SkillsJson.skills;
+  const topRow = skillsList.slice(0, 2);
+  const theMiddle = skillsList.slice(2, skillsList.length - 3);
+  const bottomRow = skillsList.slice(skillsList.length - 3, skillsList.length);
 
-    const boxSize = isDesktop ? "5rem" : isVeryTiny ? "4.2rem" : "3.3rem";
+  const boxSize = isDesktop ? '5rem' : isVeryTiny ? '4.2rem' : '3.3rem';
 
-    useEffect(() => {
-        if (isDesktop) {
-            setActiveTag("");
-        }
-    }, [isDesktop]);
+  useEffect(() => {
+    if (isDesktop) {
+      setActiveTag('');
+    }
+  }, [isDesktop]);
 
-    const handleClick = (tag: string) => {
-        if (activeTag === tag) {
-            setActiveTag("");
-        } else {
-            setActiveTag(tag);
-        }
-    };
+  const handleClick = (tag: string) => {
+    if (activeTag === tag) {
+      setActiveTag('');
+    } else {
+      setActiveTag(tag);
+    }
+  };
 
-    return (
-        <div>
-            <ScrollAnimation
-                animateIn={"fadeIn"}
-                duration={0.5}
-                animateOnce={true}
-                delay={200}
+  return (
+    <div>
+      <ScrollAnimation animateIn={'fadeIn'} duration={0.5} animateOnce={true} delay={200}>
+        <Box
+          sx={{
+            height: isDesktop ? '45.5rem' : isVeryTiny ? '46rem' : '38rem',
+            width: isDesktop ? '38rem' : '100%',
+            marginBottom: '5rem',
+            display: 'flex',
+            flexDirection: isMobile ? 'row' : 'column',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+          alignContent={'center'}
+        >
+          {!isDesktop && (
+            <Box
+              display={isMobile ? 'flex' : 'grid'}
+              sx={
+                isMobile
+                  ? {
+                      paddingRight: '5rem',
+                      flexDirection: 'column'
+                    }
+                  : {
+                      gridTemplateColumns: 'auto auto',
+                      columnGap: '0.2rem'
+                    }
+              }
             >
-                <Box
-                    sx={{
-                        height: isDesktop
-                            ? "40.5rem"
-                            : isVeryTiny
-                            ? "38rem"
-                            : "34rem",
-                        width: isDesktop ? "38rem" : "100%",
-                        marginBottom: "5rem",
-                        display: "flex",
-                        flexDirection: isMobile ? "row" : "column",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                    }}
-                    alignContent={"center"}
-                >
-                    {!isDesktop && (
-                        <Box
-                            display={isMobile ? "flex" : "grid"}
-                            sx={
-                                isMobile
-                                    ? {
-                                          paddingRight: "5rem",
-                                          flexDirection: "column",
-                                      }
-                                    : {
-                                          gridTemplateColumns: "auto auto",
-                                          columnGap: "0.2rem",
-                                      }
-                            }
-                        >
-                            <FilterButton
-                                title="Frontend"
-                                active={activeTag === "frontend"}
-                                callback={() => {
-                                    handleClick("frontend");
-                                }}
-                            />
-                            <FilterButton
-                                title="Backend"
-                                active={activeTag === "backend"}
-                                callback={() => {
-                                    handleClick("backend");
-                                }}
-                            />
+              <FilterButton
+                title='Languages'
+                active={activeTag === 'languages'}
+                callback={() => {
+                  handleClick('languages');
+                }}
+              />
+              <FilterButton
+                title='Frameworks'
+                active={activeTag === 'frameworks'}
+                callback={() => {
+                  handleClick('frameworks');
+                }}
+              />
 
-                            <FilterButton
-                                title="Tools"
-                                active={activeTag === "tools"}
-                                callback={() => {
-                                    handleClick("tools");
-                                }}
-                            />
-                            <FilterButton
-                                title="Other"
-                                active={activeTag === "api"}
-                                callback={() => {
-                                    handleClick("api");
-                                }}
-                            />
-                        </Box>
-                    )}
-                    <Box marginLeft={isDesktop ? "3rem" : "0rem"}>
-                        {/*TopRow*/}
-                        <Box
-                            sx={{
-                                ...gridStyle,
-                                gridTemplateColumns: `repeat(5, ${boxSize})`,
-                                gridAutoRows: `${boxSize}`,
-                            }}
-                        >
-                            <Box />
-                            <Box />
-                            {topRow.map((skill: Skill) => (
-                                <SkillBox
-                                    skill={skill}
-                                    activeTag={activeTag}
-                                    key={skill.id}
-                                />
-                            ))}
-                            <Box />
-                        </Box>
-                        {/*The middle parts*/}
-                        <Box
-                            sx={{
-                                ...gridStyle,
-                                gridTemplateColumns: `repeat(6, ${boxSize})`,
-                                gridAutoRows: `${boxSize}`,
-                            }}
-                        >
-                            <Box />
-                            {theMiddle.map((skill: Skill) => (
-                                <SkillBox
-                                    skill={skill}
-                                    activeTag={activeTag}
-                                    key={skill.id}
-                                />
-                            ))}
-                        </Box>
-                        {/*Bottom Row*/}
-                        <Box
-                            sx={{
-                                ...gridStyle,
-                                gridTemplateColumns: `repeat(5, ${boxSize})`,
-                                gridAutoRows: `${boxSize}`,
-                            }}
-                        >
-                            <Box />
-                            {bottomRow.map((skill: Skill) => (
-                                <SkillBox
-                                    skill={skill}
-                                    activeTag={activeTag}
-                                    key={skill.id}
-                                />
-                            ))}
-                        </Box>
-                    </Box>
-                </Box>
-            </ScrollAnimation>
-        </div>
-    );
+              <FilterButton
+                title='Tools'
+                active={activeTag === 'tools'}
+                callback={() => {
+                  handleClick('tools');
+                }}
+              />
+              <FilterButton
+                title='Libraries'
+                active={activeTag === 'libraries'}
+                callback={() => {
+                  handleClick('libraries');
+                }}
+              />
+            </Box>
+          )}
+          <Box marginLeft={isDesktop ? '3rem' : '0rem'}>
+            {/*TopRow*/}
+            <Box
+              sx={{
+                ...gridStyle,
+                gridTemplateColumns: `repeat(5, ${boxSize})`,
+                gridAutoRows: `${boxSize}`
+              }}
+            >
+              <Box />
+              <Box />
+              {topRow.map((skill: Skill) => (
+                <SkillBox skill={skill} activeTag={activeTag} key={skill.id} />
+              ))}
+              <Box />
+            </Box>
+            {/*The middle parts*/}
+            <Box
+              sx={{
+                ...gridStyle,
+                gridTemplateColumns: `repeat(6, ${boxSize})`,
+                gridAutoRows: `${boxSize}`
+              }}
+            >
+              <Box />
+              {theMiddle.map((skill: Skill) => (
+                <SkillBox skill={skill} activeTag={activeTag} key={skill.id} />
+              ))}
+            </Box>
+            {/*Bottom Row*/}
+            <Box
+              sx={{
+                ...gridStyle,
+                gridTemplateColumns: `repeat(5, ${boxSize})`,
+                gridAutoRows: `${boxSize}`
+              }}
+            >
+              <Box />
+              {bottomRow.map((skill: Skill) => (
+                <SkillBox skill={skill} activeTag={activeTag} key={skill.id} />
+              ))}
+            </Box>
+          </Box>
+        </Box>
+      </ScrollAnimation>
+    </div>
+  );
 }
