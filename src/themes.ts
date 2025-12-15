4dc4b4dc340bbd39e900091e698b908336513c0a
Patrick Palmroos <palmroos.patrick@gmail.com>
2025-12-15 20:03:46 +0200

    Update: font.

--- src/themes.ts
+++ src/themes.ts
@@ -1,50 +1,50 @@
-import { createTheme } from "@mui/material/styles";
-import "./index.css";
+import { createTheme } from '@mui/material/styles';
+import './index.css';
 
 const typography = {
-  fontFamily: "Lilita One, Roboto Flex",
+  fontFamily: 'Karla, Roboto Flex',
   h1: {
-    fontFamily: "Lilita One",
-    color: "white",
-    fontSize: "2rem",
+    fontFamily: 'Karla',
+    color: 'white',
+    fontSize: '2rem'
   },
   h2: {
-    fontFamily: "Roboto Flex",
-    color: "white",
-    fontSize: "20px",
-  },
+    fontFamily: 'Karla',
+    color: 'white',
+    fontSize: '20px'
+  }
 };
 
 const darkTheme = createTheme({
   typography: typography,
   palette: {
-    mode: "dark",
+    mode: 'dark',
     common: {
-      black: "#000000",
-      white: "#ffffff",
+      black: '#000000',
+      white: '#ffffff'
     },
     background: {
-      default: "#27242E",
+      default: '#27242E'
     },
     primary: {
-      main: "#E8CDE5",
-      light: "#B985FC",
-      dark: "#A880D2",
-      contrastText: "#BDFFA6", //Green
+      main: '#E8CDE5',
+      light: '#B985FC',
+      dark: '#A880D2',
+      contrastText: '#BDFFA6' //Green
     },
     secondary: {
-      main: "#000000",
-      light: "#000000",
-      dark: "#978EFF",
-      contrastText: "#C96BC5",
+      main: '#000000',
+      light: '#000000',
+      dark: '#978EFF',
+      contrastText: '#C96BC5'
     },
     info: {
-      main: "#DFAAFF",
-      light: "#000000",
-      dark: "#ADACFF",
-      contrastText: "#FF74D0", //red
-    },
-  },
+      main: '#DFAAFF',
+      light: '#000000',
+      dark: '#ADACFF',
+      contrastText: '#FF74D0' //red
+    }
+  }
 });
 
 export { darkTheme };
